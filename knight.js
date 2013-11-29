2bcfc5e919462132b111b9281d3f9a37f83c1b63
Maplewing <sinmaplewing@gmail.com>
2013-11-29 15:30:08 +0800

    A query can be used.

---
+++ knight.js
@@ -0,0 +1,11 @@
+var $ = knight = (function(){
+	function KnightObject(e){
+		this.elements = e;
+	}
+
+	var knight = function(query){
+		return new KnightObject(document.querySelectorAll(e));
+	}
+
+	return knight;
+})();
