08ae8efb966e65f708481db3700d1a45ccf4bbcc
Maplewing <sinmaplewing@gmail.com>
2013-11-29 20:40:47 +0800

    each & html can be used.

--- knight.js
+++ knight.js
@@ -1,11 +1,47 @@
 var $ = knight = (function(){
+
+	var knight = function(query){
+		if(query != undefined){
+			if( typeof query === "function" ){
+				window.addEventListener('load', query);
+			}
+			else{
+				return new KnightObject(document.querySelectorAll(query));
+			}
+		}
+		else{
+			
+		}
+	};
+
 	function KnightObject(e){
 		this.elements = e;
 	}
 
-	var knight = function(query){
-		return new KnightObject(document.querySelectorAll(e));
-	}
+	KnightObject.prototype.html = function(content){
+		if(content != undefined){
+			if( typeof content === "function" ){
+				this.each(function(i,e){
+					e.innerHTML = content.call(e, i, e.innerHTML);
+				});
+			}
+			else{
+				this.each(function(i, e){
+					e.innerHTML = content;
+				});
+			}
+			return this;
+		}
+		else{
+			return this.elements[0].innerHTML;
+		}
+	};
+
+	KnightObject.prototype.each = function(f){
+		for( var i = 0 ; i < this.elements.length ; ++i ){
+			f.call(this.elements[i], i, this.elements[i]);
+		}
+	};
 
 	return knight;
 })();
